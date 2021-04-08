cf8b10b2226e072d65aae3e7863a3959c0e8a997
yizhakg <yizhakg.gmail.com>
2021-04-08 14:20:08 +0300

    update changes

--- main.js
+++ main.js
@@ -102,6 +102,7 @@ window.onload = () => {
     document.body.scrollTop = 0; // For Safari
     document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
   });
+  
   document.querySelector(".downArw").addEventListener("click", () => {
     scrollToDiv(`cont1`);
   });
